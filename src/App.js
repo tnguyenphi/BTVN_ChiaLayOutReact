56e1a76dc73cd9f0b5957eef6cb0c06179843ecf
NguyenPhi <nguyenphidhbk@gmail.com>
2021-11-28 10:45:55 +0700

    bai tap chia layout React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import BaiTap1 from './baitap1';
+import BaiTap2 from './baitap2';
+import BaiTapChiaLayout from './BaiTapChiaLayout'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <BaiTap1/> */}
+      {/* <BaiTap2/> */}
+      <BaiTapChiaLayout/>
     </div>
   );
 }
